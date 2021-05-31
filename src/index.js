98003c26566199125fc3f3382b5fae74c3493e1f
sideeg-ahmed <43040213+sideeg-ahmed@users.noreply.github.com>
2021-05-31 16:30:42 +0300

    deleted dead code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,3 @@
-// import * as React from "react"
-
-// // 1. import `ChakraProvider` component
-// import { ChakraProvider } from "@chakra-ui/react"
-
-// function App({ Component }) {
-//   // 2. Use at the root of your app
-//   return (
-//     <ChakraProvider>
-//       <Component />
-//     </ChakraProvider>
-//   )
-// }
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
